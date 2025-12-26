ad789d9e5afa367dc97ece3f3f5feab886210577
0xsuk <iam0xsuk@gmail.com>
2025-12-27 03:53:17 +0900

    起動時間短縮

--- init.el
+++ init.el
@@ -13,6 +13,11 @@
 ; $$ to grab output of last command
 ; copy output of last command
 
+;;; 起動処理中はMagic File Name機能をオフ
+;; https://zenn.dev/zk_phi/books/cba129aacd4c1418ade4/viewer/dcebc13578d42055f8a4#magic-file-name-%E3%82%92%E4%B8%80%E6%99%82%E7%9A%84%E3%81%AB%E7%84%A1%E5%8A%B9%E3%81%AB%E3%81%99%E3%82%8B
+(defconst my/saved-file-name-handler-alist file-name-handler-alist)
+(setq file-name-handler-alist nil)
+
 ;; tab
 ;; (setq tab-bar-show nil)
 ;; (tab-bar-mode)
@@ -80,3 +85,7 @@
 (message "INIT COMPLETE")
 
 ;; (profiler-report)
+
+
+;;; 起動処理終了でMagic File Name機能を復元
+(setq file-name-handler-alist my/saved-file-name-handler-alist)
