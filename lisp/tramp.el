a9a477a110ceac4140aeec161abee5ff3eb26658
0xsuk <iam0xsuk@gmail.com>
2026-01-31 02:35:44 +0900

    remove tramp

--- lisp/tramp.el
+++ lisp/tramp.el
@@ -4,20 +4,21 @@
   :config
 
   ;; Enable TRAMP direct async processes for a given protocol/host profile.
-  (connection-local-set-profile-variables
-   'remote-direct-async-process
-   '((tramp-direct-async-process . t)))
+  (defun my-tramp-setup ()
+    (connection-local-set-profile-variables
+     'remote-direct-async-process
+     '((tramp-direct-async-process . t)))
 
-  ;; Example: enable for scp protocol (adjust to your setup).
-  (connection-local-set-profiles
-   '(:application tramp :protocol "scp")
-   'remote-direct-async-process)
+    ;; Example: enable for scp protocol (adjust to your setup).
+    (connection-local-set-profiles
+     '(:application tramp :protocol "scp")
+     'remote-direct-async-process)
 
-  ;; Magit tweak often needed when tramp-direct-async-process is enabled.
-  ;; (setq magit-tramp-pipe-stty-settings 'pty)
-  
-  (setq remote-file-name-inhibit-locks t
-      remote-file-name-inhibit-auto-save-visited t
-      tramp-use-scp-direct-remote-copying t ; obsolete?
-      )
+    ;; Magit tweak often needed when tramp-direct-async-process is enabled.
+    ;; (setq magit-tramp-pipe-stty-settings 'pty)
+    
+    (setq remote-file-name-inhibit-locks t
+          remote-file-name-inhibit-auto-save-visited t
+          tramp-use-scp-direct-remote-copying t ; obsolete?
+          ))
   )
